99f381644502e6e1730f0a0f485c5ab3d9706022
suneelsuthar <suneelsuther123@gmail.com>
2020-02-17 15:42:41 +0500

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    "imagebaseurl":"https://avatars0.githubusercontent.com/u/47412472?s=40&v=4",
+    "name": "Suneel kumar",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://avatars0.githubusercontent.com/u/47412472?s=40&v=4",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://avatars0.githubusercontent.com/u/47412472?s=40&v=4",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a final year student of Bechlor of Science at Sindh Universty Jamshoro. I am a self taught MERN Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    
+    "address":"Karachi,Sindh,Pakistan",
+    "website":"https://avatars0.githubusercontent.com/u/47412472?s=40&v=4",
+    "education":[
+      {
+        "UniversityName":"Sindh University jamshore",
+        "specialization":"Bsc",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javasript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Gatsby"
+      },
+      {
+        "skillname":"React Native"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
